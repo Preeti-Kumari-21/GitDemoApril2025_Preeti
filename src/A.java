ee903b0e70fb6e7a2f2c8fcc68f1716cc6876601
Preeti <preeti.kumari241116@gmail.com>
2025-04-04 07:56:17 +0530

    adding getter and setter

--- src/A.java
+++ src/A.java
@@ -1,4 +1,12 @@
 public class A {
     private int empId;
     private String employeeName;
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
 }
